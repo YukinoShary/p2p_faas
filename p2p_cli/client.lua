f414de5423725ba234063cc85c40d34addf27e3d
YukinoShary <shary490106898@gmail.com>
2023-07-29 19:53:48 +0000

    edited file path in client.lua

--- p2p_cli/client.lua
+++ p2p_cli/client.lua
@@ -1,7 +1,7 @@
 print("start client")
 local target_ip = "http://192.168.128.22:8082"
-package.cpath = "/home/shary/program/peer2peer/client.so"
-local func_lib = require("p2p_client")
+package.cpath = "client.so"
+local func_lib = require("p2p_client_edit")
 local file = io.open("sendFile", "r")
 if nil == file then
     print("open file fail")
